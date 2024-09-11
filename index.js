0753b718c4a0e26070cd6534c058159d414c9b3e
Guilherme <guilh@Guilherme>
2024-09-11 16:21:53 -0400

    fundamentos004

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 function start(){
-    let count=0
-    while(count<10){
-        console.log(count)
-        count=count+1
-}
+    while(true){
+        let option='sair'
+        switch(option){
+            case'cadastrar':
+                console.log('vamos cadastrar')
+                break
+            case'listar':
+                console.log('vamos listar')
+                break
+            case'sair':
+                return
 
+        }
+    }
 }
 start()
