0cf4a42d3680468503671c3ac2a415799a2bdf16
Guilherme <guilh@Guilherme>
2024-09-11 15:38:51 -0400

    fundametos002

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-//hello world
-let mensagem = "hello world"
-{
-    const mensagem='hello me'
-console.log(mensagem)
+
+let meta = {
+    value:'ler um livro por mes',
+    checked: false
+
 }
-console.log(mensagem)
+let metas = [
+    meta,{
+    value:'caminhar 20 min todo dia',
+    checked:true
+    }
+]
+console.log(metas[0].value)
