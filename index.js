ec322cfd899a9bbc49cd10a8b0ed31fc33e42a39
Guilherme <guilh@Guilherme>
2024-09-12 23:15:10 -0400

    listar metas

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const{select, input }=require('@inquirer/prompts')
+const{select, input, checkbox }=require('@inquirer/prompts')
 
 let meta={value:'Tomar 3L litros de agua por dia',
     checked: false
@@ -12,6 +12,28 @@ const cadastrarMeta=async()=>{
         return
     }
     metas.push({value:meta, checked:false})
+}
+
+const listarMetas=async()=>{
+    const respostas=await checkbox({
+        message: 'Use as setas para mudar de meta, o espaco marcar ou desmarcar, e o Enter para finalizar essa etapa.',
+        choices:[...metas] 
+    })
+
+    if(respostas.length==0){
+        console.log('Nenhuma meta selecionada')
+        return
+    }
+
+    metas.forEach((m)=>{m.checked=false})
+
+    respostas.forEach((resposta)=>{
+        const meta=metas.find((m)=>{
+            return m.value==resposta
+    })
+        meta.checked=true
+    })
+    console.log('Meta(s) marcada(s) como concluida(s)')
 }
 
 async function start() {
@@ -41,7 +63,7 @@ async function start() {
                 console.log(metas)
                 break
             case 'listar':
-                console.log('vamos listar')
+                await listarMetas()
                 break
             case 'sair':
                 console.log('Ate a proxima')
