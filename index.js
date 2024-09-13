c64659a0b21a6fee8ccafb79a3b6f9e869057a23
Guilherme <guilh@Guilherme>
2024-09-12 22:27:07 -0400

    cadastra meta

--- index.js
+++ index.js
@@ -1,34 +1,49 @@
-const{select}=require('@inquirer/prompts')
+const{select, input }=require('@inquirer/prompts')
 
-const start = async () =>{
-    while(true){
-        
-        const option= await select({
+let meta={value:'Tomar 3L litros de agua por dia',
+    checked: false
+}
+let metas=[meta]
+
+const cadastrarMeta=async()=>{
+    const meta=await input({message:'Digite sua meta:'})
+    if(meta.length==0){
+        console.log('A meta nao pode ser vazia.')
+        return
+    }
+    metas.push({value:meta, checked:false})
+}
+
+async function start() {
+    while (true) {
+
+        const option = await select({
             message: 'Menu >',
-            choices:[
+            choices: [
                 {
-                    name:'Cadastrar meta',
-                    value:'cadastrar'
-                },
-                { 
-                    name:'Listar meta',
-                    value:'listar'
+                    name: 'Cadastrar meta',
+                    value: 'cadastrar'
                 },
                 {
-                    name:'Sair',
-                    value:'sair'
+                    name: 'Listar meta',
+                    value: 'listar'
+                },
+                {
+                    name: 'Sair',
+                    value: 'sair'
                 }
             ]
         })
 
-        switch(option){
-            case'cadastrar':
-                console.log('vamos cadastrar')
+        switch (option) {
+            case 'cadastrar':
+                await cadastrarMeta()
+                console.log(metas)
                 break
-            case'listar':
+            case 'listar':
                 console.log('vamos listar')
                 break
-            case'sair':
+            case 'sair':
                 console.log('Ate a proxima')
                 return
 
